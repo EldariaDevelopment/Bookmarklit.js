ebf67d59749d60a6918693a67935687a2d0ad998
Almond <93007463+EldariaDevelopment@users.noreply.github.com>
2022-09-19 20:12:19 -0700

    Created.

---
+++ Main.js
@@ -0,0 +1,46 @@
+<script>
+window.onload = function Start1() {
+  let Command = prompt("Enter Entry");
+  let x = 1;
+
+  switch (Command) {
+    case "time":
+      alert(Date());
+      break;
+    case "Calc":
+      Calc()
+      break;
+    default:
+      text = "No value found";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+
+function Calc() {
+ 	let CalcX = prompt("Enter first number");
+    let CalcY = prompt("Enter Operator");
+	let CalcZ = prompt("Enter Second number");
+  switch(CalcY) {
+    case "**":
+      resultNum = (CalcX ** CalcZ);
+      alert(resultNum)
+      break;
+    case "*":
+      resultNum = (CalcX * CalcZ);
+      alert(resultNum)
+      break;
+    case "+":
+      resultNum = (CalcX + CalcZ);
+      alert(resultNum)
+      break;
+    case "-":
+      resultNum = (CalcX - CalcZ);
+      alert(resultNum)
+      break;
+    case "/":
+      resultNum = (CalcX / CalcZ);
+      alert(resultNum)
+      break;
+  }
+}
+</script>
